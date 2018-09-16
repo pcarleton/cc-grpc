6fc69544d8a566b7ebc39d6eac7992fcd7af7b21
Paul Carleton <paulcarletonjr@gmail.com>
2018-09-16 14:13:20 -0700

    Add plaid and update build files

---
+++ plaid/plaid.go
@@ -0,0 +1,327 @@
+package plaid
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
+)
+
+const (
+	DevURL     = "https://development.plaid.com"
+	SandboxURL = "https://sandbox.plaid.com"
+	DateFmt    = "2006-01-02"
+)
+
+type Client struct {
+	baseURL  string
+	client   *http.Client
+	clientID string
+	secret   string
+}
+
+func NewClient(clientID, secret, baseURL string) Client {
+	return Client{
+		baseURL:  baseURL,
+		client:   new(http.Client),
+		clientID: clientID,
+		secret:   secret,
+	}
+}
+
+type UpdateAccessTokenRequest struct {
+	ClientID    string `json:"client_id"`
+	Secret      string `json:"secret"`
+	AccessToken string `json:"access_token_v1"`
+}
+
+type UpdateAccessTokenResponse struct {
+	AccessToken string `json:"access_token"`
+	RequestID   string `json:"request_id"`
+}
+
+func (c *Client) UpdateAccessToken(accessToken string) (UpdateAccessTokenResponse, error) {
+	endpoint := "/item/access_token/update_version"
+
+	req := UpdateAccessTokenRequest{c.clientID, c.secret, accessToken}
+
+	resp := UpdateAccessTokenResponse{}
+	err := c.post(endpoint, req, &resp)
+
+	if err != nil {
+		return resp, err
+	}
+
+	return resp, nil
+}
+
+type ApiError struct {
+	Response *ErrorResponse
+}
+
+func (e ApiError) Error() string {
+	return fmt.Sprintf("%+v", e.Response)
+}
+
+func (c *Client) post(endpoint string, req interface{}, resp interface{}) error {
+
+	jsonText, err := json.Marshal(req)
+	if err != nil {
+		return err
+	}
+
+	target := c.baseURL + endpoint
+
+	postResp, err := c.client.Post(target, "application/json",
+		bytes.NewReader(jsonText))
+
+	if err != nil {
+		return err
+	}
+
+	respBody, err := ioutil.ReadAll(postResp.Body)
+
+	if err != nil {
+		return err
+	}
+
+	if postResp.StatusCode != 200 {
+		errResp := ErrorResponse{}
+		if err := json.Unmarshal(respBody, &errResp); err != nil {
+			return err
+		}
+
+		return ApiError{&errResp}
+	}
+
+	if err := json.Unmarshal(respBody, resp); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+type TransactionRequest struct {
+	ClientID    string                     `json:"client_id"`
+	Secret      string                     `json:"secret"`
+	AccessToken string                     `json:"access_token"`
+	StartDate   string                     `json:"start_date"`
+	EndDate     string                     `json:"end_date"`
+	Options     *TransactionRequestOptions `json:"options,omitempty"`
+}
+
+type TransactionRequestOptions struct {
+	AccountIDs []string `json:"account_ids,omitempty"`
+	Count      int      `json:"count,omitempty"`
+	Offset     int      `json:"offset,omitempty"`
+}
+
+type BalanceRequest struct {
+	ClientID    string `json:"client_id"`
+	Secret      string `json:"secret"`
+	AccessToken string `json:"access_token"`
+}
+
+type BalanceResponse struct {
+	Accounts  []Account `json:"accounts"`
+	Item      Item      `json:"item"`
+	RequestID string    `json:"request_id"`
+}
+
+type Balance struct {
+	Available float64 `json:"available"`
+	Current   float64 `json:"current"`
+	Limit     float64 `json:"limit"`
+}
+
+type Account struct {
+	ID            string  `json:"account_id"`
+	ItemID        string  `json:"item_id"`
+	InstitutionID string  `json:"institution_id"`
+	Balances      Balance `json:"balances"`
+	Name          string  `json:"name"`
+	Mask          string  `json:"mask"`
+	OfficialName  string  `json:"official_name"`
+	Type          string  `json:"type"`
+	Subtype       string  `json:"subtype"`
+}
+
+type TransactionResponse struct {
+	Accounts          []Account     `json:"accounts"`
+	Transactions      []Transaction `json:"transactions"`
+	Item              Item          `json:"item"`
+	RequestID         string        `json:"request_id"`
+	TotalTransactions int           `json:"total_transactions"`
+}
+
+type ErrorResponse struct {
+	Type           string `json:"error_type"`
+	Code           string `json:"error_code"`
+	Message        string `json:"error_message"`
+	DisplayMessage string `json:"display_message"`
+}
+
+type Item struct {
+	AvailableProducts []string      `json:"available_products"`
+	BilledProducts    []string      `json:"billed_products"`
+	Error             ErrorResponse `json:"error"`
+	InstitutionID     string        `json:"institution_id"`
+	ItemID            string        `json:"item_id"`
+}
+
+type Transaction struct {
+	ID           string   `json:"transaction_id"`
+	AccountID    string   `json:"account_id"`
+	Category     []string `json:"category"`
+	CategoryID   string   `json:"category_id"`
+	Type         string   `json:"transaction_type"`
+	Amount       float64  `json:"amount"`
+	Date         string   `json:"date"`
+	Pending      bool     `json:"pending"`
+	AccountOwner string   `json:"account_owner"`
+	Name         string   `json:"name"`
+}
+
+type PublicTokenRequest struct {
+	ClientID    string `json:"client_id"`
+	Secret      string `json:"secret"`
+	AccessToken string `json:"access_token"`
+}
+
+type PublicTokenResponse struct {
+	RequestID   string `json:"request_id"`
+	PublicToken string `json:"public_token"`
+}
+
+func (c *Client) transactions(accessToken string, startDate, endDate time.Time, options TransactionRequestOptions) (TransactionResponse, error) {
+	endpoint := "/transactions/get"
+
+	request := TransactionRequest{
+		ClientID:    c.clientID,
+		Secret:      c.secret,
+		AccessToken: accessToken,
+		StartDate:   startDate.Format(DateFmt),
+		EndDate:     endDate.Format(DateFmt),
+		Options:     &options,
+	}
+
+	resp := TransactionResponse{}
+	err := c.post(endpoint, request, &resp)
+	if err != nil {
+		return resp, err
+	}
+
+	return resp, nil
+}
+
+func (c *Client) Transactions(accessToken string, startDate, endDate time.Time) (TransactionResponse, error) {
+	options := TransactionRequestOptions{}
+	resp, err := c.transactions(accessToken, startDate, endDate, options)
+
+	if err != nil {
+		return resp, err
+	}
+
+	seenNum := len(resp.Transactions)
+	totalNum := resp.TotalTransactions
+
+	if seenNum == totalNum {
+		return resp, nil
+	}
+
+	// Fetch all transactions in range with repeated requests
+	allTransactions := make([]Transaction, totalNum)
+	copy(allTransactions[0:], resp.Transactions)
+
+	for seenNum < totalNum {
+		options = TransactionRequestOptions{Offset: seenNum}
+		resp, err = c.transactions(accessToken, startDate, endDate, options)
+
+		if err != nil {
+			return resp, err
+		}
+
+		if len(resp.Transactions) == 0 {
+			return resp, err
+		}
+
+		copy(allTransactions[seenNum:], resp.Transactions)
+		seenNum += len(resp.Transactions)
+	}
+
+	resp.Transactions = allTransactions
+
+	return resp, nil
+}
+
+func (c *Client) CreatePublicToken(accessToken string) (PublicTokenResponse, error) {
+	endpoint := "/item/public_token/create"
+
+	request := PublicTokenRequest{
+		ClientID:    c.clientID,
+		Secret:      c.secret,
+		AccessToken: accessToken,
+	}
+
+	resp := PublicTokenResponse{}
+	err := c.post(endpoint, request, &resp)
+	if err != nil {
+		return resp, err
+	}
+
+	return resp, nil
+}
+
+type ExchangeRequest struct {
+	ClientID    string `json:"client_id"`
+	Secret      string `json:"secret"`
+	PublicToken string `json:"public_token"`
+}
+
+type ExchangeResponse struct {
+	AccessToken string `json:"access_token"`
+	ItemID      string `json:"item_id"`
+	ErrorResponse
+}
+
+func (c *Client) Exchange(publicToken string) (ExchangeResponse, error) {
+	endpoint := "/item/public_token/exchange"
+
+	request := ExchangeRequest{
+		ClientID:    c.clientID,
+		Secret:      c.secret,
+		PublicToken: publicToken,
+	}
+
+	resp := ExchangeResponse{}
+	err := c.post(endpoint, request, &resp)
+	if err != nil {
+		return resp, err
+	}
+
+	if resp.Message != "" {
+		return resp, fmt.Errorf(resp.Message)
+	}
+
+	return resp, nil
+}
+
+func (c *Client) RetrieveBalance(accessToken string) (BalanceResponse, error) {
+	endpoint := "/accounts/balance/get"
+
+	request := BalanceRequest{
+		ClientID:    c.clientID,
+		Secret:      c.secret,
+		AccessToken: accessToken,
+	}
+
+	resp := BalanceResponse{}
+	err := c.post(endpoint, request, &resp)
+	if err != nil {
+		return resp, err
+	}
+
+	return resp, nil
+}
